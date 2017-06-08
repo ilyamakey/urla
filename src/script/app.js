14a8632e2ee5b1b9a5cbb819d9e70b3c3431217c
ilya <ilyamakey@gmail.com>
2017-06-08 08:51:26 +0300

    make ajax

--- src/script/app.js
+++ src/script/app.js
@@ -15,4 +15,25 @@
 //     xhr.send();
 //   },
 // });
-console.log('hello world');
+var url = 'https://api.github.com/repos/ilyamakey/urla/contents/src/pics';
+var tablet = document.querySelector('.tablet');
+var xhr = new XMLHttpRequest();
+var data = ['Gop', 'TOP', 'Hlop'];
+var template = function (obj) {
+  var div = document.createElement('div');
+  div.className = "tablet__slide";
+  div.innerHTML = '<img src="' + obj.html_url + '"><h3>JOPEN</h3></div>';
+  return div;
+};
+
+xhr.open('GET', url, true);
+
+xhr.onload = function() {
+  var data = JSON.parse(xhr.response);
+  data.forEach(function(elem) {
+    tablet.appendChild(template(elem));
+    console.log(elem);
+  });
+};
+// console.log(data);
+xhr.send();
