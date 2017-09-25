b439ce6631dce1d22964b806d8e1f45d8e6f932e
ilya <ilyamakey@gmail.com>
2017-09-26 00:03:05 +0300

    minor fixes

--- src/script/modules/AjaxRequest.js
+++ src/script/modules/AjaxRequest.js
@@ -3,28 +3,6 @@ function AjaxRequest(data) {
   this.data = data; //delete
 };
 
-// AjaxRequest.prototype.clear = function(node) {
-//   var node = document.querySelector(node);
-//   var divs = node.getElementsByTagName('div');
-//
-//   for (var i = 0; i < divs.length; /* no interator needed*/) {
-//     node.removeChild(divs[i]);
-//   };
-//
-//   return this; // for chaining
-//
-// };
-//
-// AjaxRequest.prototype.render = function (node, template) {
-//
-//   var node = document.querySelector(node);
-//
-//   this.data.forEach(function(elem) {
-//     node.appendChild(template.create(elem));
-//   });
-//
-// };
-
 AjaxRequest.prototype.load = function(url, callback) {
   var self = this;
 
